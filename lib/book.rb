f85e3556d5c16a655bc318d192305cb898c01acc
Angelo Spampinato <spampinatodesign@gmail.com>
2019-01-30 11:23:38 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book 
+	attr_accessor :author, :page_count, :genre
+	attr_reader :title
 
+	def initialize(title)
+		@title = title
+	end
+
+	# Replaced by attr_reader
+	# def title
+	# 	@title
+	# end
+	
+	def turn_page
+		puts "Flipping the page...wow, you read fast!"
+	end
+end
 
